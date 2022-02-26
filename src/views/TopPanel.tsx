8138d52499df2b9780e7426fa467512cd9609464
mikhailcazi <mikhailcazi@gmail.com>
2022-02-26 11:43:43 +0000

    Styling

--- src/views/TopPanel.tsx
+++ src/views/TopPanel.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 export class TopPanel extends React.Component{
+
     todayDate: string | number | readonly string[] | undefined;
 
-    
     componentDidMount() {
+        var local = new Date();
+        this.todayDate = local.toISOString().slice(0, 10)
+    }
 
-            var local = new Date();
-this.todayDate = local.toISOString().slice(0, 10)
-
+    valueChanged(event: any) {
+        if(event) {
+            this.todayDate = event.target.value;
+        }
     }
 
     render() {
@@ -17,9 +21,9 @@ this.todayDate = local.toISOString().slice(0, 10)
                 <div className="top-float">
 
                 <i className="bx bxs-file-pdf pdf-icon"/>
-                |
+
                 <label className="date-range-label">Date:</label>
-                <input value={this.todayDate} type={'date'} className="date-picker"/>
+                <input value={this.todayDate} type={'date'} className="date-picker" onChange={this.valueChanged.bind(this)}/>
                 </div>
                 
             </div>
